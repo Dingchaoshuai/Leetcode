f816b48d6b4c7e82476c3310118e0ec4b06756a3
Dingchaoshuai <dc.vip.1998@qq.com>
2020-09-18 13:25:21 +0800

    回文数

---
+++ leetcode/editor/cn/[9]回文数.java
@@ -0,0 +1,43 @@
+//判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。 
+//
+// 示例 1: 
+//
+// 输入: 121
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 输入: -121
+//输出: false
+//解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+// 
+//
+// 示例 3: 
+//
+// 输入: 10
+//输出: false
+//解释: 从右向左读, 为 01 。因此它不是一个回文数。
+// 
+//
+// 进阶: 
+//
+// 你能不将整数转为字符串来解决这个问题吗？ 
+// Related Topics 数学 
+// 👍 1234 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isPalindrome(int x) {
+        for(int i =0;i<nums.length;i++){
+            for(int j =i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
